50c9ef07d277168405eb8deb0e1154e846d57c81
Adnan Reza <adnan.reza@outlook.com>
2019-07-26 15:42:50 -0700

    Implement Show Git Repos function

--- github.js
+++ github.js
@@ -2,19 +2,31 @@ class GitHub {
   constructor() {
     this.client_id = "Iv1.5c16e2308ea9c73c";
     this.client_secret = "2f86245b3144e580c07e043a8316f7a2772a54cc";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
   }
 
   async getUser(user) {
     const profileResponse = await fetch(
-      `https://api.github.com/users/${user}?client_id?=${
+      `https://api.github.com/users/${user}?client_id=${
         this.client_id
       }&client_secret=${this.client_secret}`
     );
 
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${
+        this.repos_count
+      }&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${
+        this.client_secret
+      }`
+    );
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile
+      profile,
+      repos
     };
   }
 }
